809c40c95ea7980e45381cafd87fc1bce2f959b9
Krishna Karumanchi <karumank@mail.gvsu.edu>
2026-07-25 23:25:13 -0500

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        path = []
+
+        def dfs(idx):
+            if idx == len(nums):
+                result.append(path.copy())
+                return 
+            path.append(nums[idx])
+            dfs(idx + 1)
+            path.pop()
+            dfs(idx + 1)
+        
+        dfs(0)
+        return result
