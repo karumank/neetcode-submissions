89e17a1d04d896a15b06a55718535844411edc11
Krishna Karumanchi <karumank@mail.gvsu.edu>
2026-08-23 00:56:15 -0500

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if len(nums) <= 2:
+            return max(nums)
+        maxAmountIncludeOne = [0] * (n - 1)
+        maxAmountExcludeOne = [0] * (n - 1)
+        
+        prevIncludeOne, currIncludeOne = nums[0], max(nums[0], nums[1])
+
+        prevExcludeOne, currExcludeOne = nums[1], max(nums[1], nums[2])
+
+        for i in range(2, n - 1):
+            prevIncludeOne,  currIncludeOne = currIncludeOne, max(currIncludeOne, prevIncludeOne + nums[i])
+        
+        for i in range(2, n - 1):
+            prevExcludeOne, currExcludeOne = currExcludeOne, max(currExcludeOne, prevExcludeOne + nums[i + 1])
+        
+        
+        return max(currIncludeOne, currExcludeOne)
+
